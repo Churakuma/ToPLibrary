3dc127232ed2d5b0ee40ccb67e7a08abcbbf6f7b
Zubair <zubair99ss@hotmail.com>
2024-02-22 22:16:12 +0800

    Add books to library to test display

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
-const myLibrary = [];
+const myLibrary = [
+    {
+        title: "Prince of Fools",
+        author: "Mark Lawrence",
+        pages: 245,
+        read: false
+    },
+    {
+        title: "Harry Potter and the Philosopher's Stone",
+        author: "J.K Rowling",
+        pages: 479,
+        read: true
+    }
+];
+
+function Book() {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+};
 
 const modal = document.getElementById('my__modal');
 const addBookButton = document.getElementById('add__book__button');
@@ -6,27 +26,26 @@ const span = document.getElementsByClassName('close')[0];
 
 addBookButton.onclick = function() {
     modal.style.display = 'flex';
-}
+};
 
 span.onclick = function() {
     modal.style.display = 'none';
-}
+};
 
 // If user clicks outside of modal, close the modal
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = 'none';
-    }
-}
+    };
+};
 
-function Book() {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-}
+function displayBooks(myLibrary) {
+    for (Book in myLibrary) {
+
+    };
+};
 
 function addBookToLibrary(book) {
     myLibrary.push(book);
     console.log("Added " + book.title + " to the library");
-}
+};
