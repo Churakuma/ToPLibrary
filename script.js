3d5bd8627025811c84401424fc2b46dfafa25427
Zubair <zubair99ss@hotmail.com>
2024-02-22 22:03:20 +0800

    Style model and add open/close logic

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 const myLibrary = [];
 
+const modal = document.getElementById('my__modal');
+const addBookButton = document.getElementById('add__book__button');
+const span = document.getElementsByClassName('close')[0];
+
+addBookButton.onclick = function() {
+    modal.style.display = 'flex';
+}
+
+span.onclick = function() {
+    modal.style.display = 'none';
+}
+
+// If user clicks outside of modal, close the modal
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = 'none';
+    }
+}
+
 function Book() {
     this.title = title;
     this.author = author;
